e5654b47b00a0b0123487cddb14d2c9fc0baf43f
YannJollien <48057181+YannJollien@users.noreply.github.com>
2019-03-25 20:06:34 +0100

    Fragments

--- app/src/main/java/com/example/coffeeproject2/ui/storage/StorageViewTest.java
+++ app/src/main/java/com/example/coffeeproject2/ui/storage/StorageViewTest.java
@@ -1,20 +1,33 @@
 package com.example.coffeeproject2.ui.storage;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.example.coffeeproject2.R;
+import com.example.coffeeproject2.StorageViewModel;
+import com.example.coffeeproject2.adapter.StorageAdapter;
+import com.example.coffeeproject2.database.entity.Storage;
+
+import java.util.List;
 
 public class StorageViewTest extends AppCompatActivity {
+
+    private StorageViewModel storageViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage_view_test);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.tool_bar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.storage_toolbar);
         setSupportActionBar(toolbar);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("All"));
@@ -28,7 +41,7 @@ public class StorageViewTest extends AppCompatActivity {
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
+
             }
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
@@ -39,5 +52,6 @@ public class StorageViewTest extends AppCompatActivity {
 
             }
         });
+
     }
 }
