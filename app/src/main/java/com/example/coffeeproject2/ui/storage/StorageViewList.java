c2e018c8654077affbf68e3e611491d46bb5ce86
AgronAsani <agron.asani@students.hevs.ch>
2019-04-16 14:39:49 +0200

    Firebase Delete

--- app/src/main/java/com/example/coffeeproject2/ui/storage/StorageViewList.java
+++ app/src/main/java/com/example/coffeeproject2/ui/storage/StorageViewList.java
@@ -98,19 +98,13 @@ public class StorageViewList extends AppCompatActivity {
 
             @Override
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-                Toast.makeText(getApplicationContext(), "Deleted", Toast.LENGTH_SHORT).show();
+                int position = viewHolder.getAdapterPosition();
 
-                int position = viewHolder.getAdapterPosition(); // this is how you can get the position
-                System.out.println(position);
-                Storage store = storageList.get(position);
-                System.out.println(store.getId() + " ist die ID und " + store.getAmount() + " ist die Menge");
+                Storage storage = adapter.getStorage(position);
 
-                Storage storage = adapter.getStorage(position); // You will have your own class ofcourse.
-                System.out.println(storage.getId());
-
-                // then you can delete the object
-                reference.child("Storage").child(storage.getId()).setValue(null);// setting the value to null will just delete it from the database.
+                reference.child(storage.getId()).removeValue();
                 startActivity(new Intent(StorageViewList.this, StorageViewActivity.class));
+                Toast.makeText(getApplicationContext(), "Deleted", Toast.LENGTH_SHORT).show();
             }
         }).attachToRecyclerView(recyclerView);
 
